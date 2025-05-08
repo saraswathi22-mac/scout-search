e47a15f982f1b5ad4a381d254c79c4da885ed59e
M A Saraswathi <saraswathi.mac@gmail.com>
2025-05-08 20:04:50 +0530

    Updated Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,12 +8,15 @@ function Home() {
   return (
     <div className="home">
       <header className="header">
-        <div></div>
-        <nav className="right">
+        <div className="header__left">
+          <Link to="/">About</Link>
+          <Link to="/">Store</Link>
+        </div>
+        <nav className="header__right">
           <Link to="/">Gmail</Link>
           <Link to="/">Images</Link>
           <AppsIcon aria-label="Google apps menu" />
-          <Avatar alt="User profile" />
+          <Avatar alt="User profile" className="header__avatar" />
         </nav>
       </header>
 
@@ -22,10 +25,24 @@ function Home() {
           src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
           alt="Google logo"
         />
-        <div className="search_input">
-          <Search hideButtons={false} />
-        </div>
+        <Search hideButtons={false} />
       </main>
+
+      <footer className="footer">
+        <div className="footer__region">India</div>
+        <div className="footer__links">
+          <div className="footer__left">
+            <Link to="/">Advertising</Link>
+            <Link to="/">Business</Link>
+            <Link to="/">How Search works</Link>
+          </div>
+          <div className="footer__right">
+            <Link to="/">Privacy</Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Settings</Link>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }

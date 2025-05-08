69dc317082631e735c1eb88825a93a203f397a50
M A Saraswathi <saraswathi.mac@gmail.com>
2025-05-08 16:07:21 +0530

    Updated dropdown

--- src/components/DropDown/Dropdown.jsx
+++ src/components/DropDown/Dropdown.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import "./DropDown.css"
+import React from "react";
+import "./DropDown.css";
 
-const Dropdown = ({show}) => {
+const items = ["Maps", "Books", "Flights", "Finance"];
+
+const Dropdown = ({ show }) => {
   return (
-    <div className={`${show ? "block" : "hidden"} dropdown`}>
-        <div className="dropdown-content">
-        <button>Maps</button>
-      </div>
-      <div className="dropdown-content">
-        <button>Books</button>
-      </div>
-      <div className="dropdown-content">
-        <button>Flights</button>
-      </div>
-      <div className="dropdown-content">
-        <button>Finance</button>
-      </div>
+    <div className={`dropdown ${show ? "block" : "hidden"}`}>
+      {items.map((item) => (
+        <div className="dropdown-content" key={item}>
+          <button>{item}</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;

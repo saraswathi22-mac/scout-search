8f2f127a3116ef94072a9b505cd865a00bb025a5
M A Saraswathi <saraswathi.mac@gmail.com>
2025-05-08 15:31:39 +0530

    Updated Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,24 +7,25 @@ import Search from "../../components/Search/Search";
 function Home() {
   return (
     <div className="home">
-      <div className="header">
+      <header className="header">
         <div></div>
-        <div className="right">
+        <nav className="right">
           <Link to="/">Gmail</Link>
           <Link to="/">Images</Link>
-          <AppsIcon />
-          <Avatar />
-        </div>
-      </div>
-      <div className="body">
+          <AppsIcon aria-label="Google apps menu" />
+          <Avatar alt="User profile" />
+        </nav>
+      </header>
+
+      <main className="body">
         <img
           src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-          alt=""
+          alt="Google logo"
         />
         <div className="search_input">
           <Search hideButtons={false} />
         </div>
-      </div>
+      </main>
     </div>
   );
 }

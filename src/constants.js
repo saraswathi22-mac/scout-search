c208b80a50ae12df6d8b35fecd73af9987c10655
M A Saraswathi <saraswathi.mac@gmail.com>
2025-05-10 20:23:13 +0530

    Updated search page and constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const searchPageOptionsLeft = [
+  "All",
+  "Images",
+  "Shopping",
+  "Videos",
+  "News",
+];
+
+export const searchPageMore = ["Maps", "Web", "Books"];
+
+export const searchPageOptionsRight = ["Tools"];
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -6,6 +6,11 @@ import { useStateValue } from "../../context/StateProvider";
 import { useGoogleSearch } from "../../services/useGoogleSearch";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Dropdown from "../../components/DropDown/Dropdown";
+import {
+  searchPageMore,
+  searchPageOptionsLeft,
+  searchPageOptionsRight,
+} from "../../constants";
 
 function SearchPage() {
   const { term, dispatch } = useStateValue();
@@ -14,35 +19,25 @@ function SearchPage() {
 
   return (
     <div className="searchPage">
-      <div className="sP_header">
+      <header className="sP_header">
         <Link to="/">
           <img
             className="sP_logo"
             src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-            alt=""
+            alt="Google Logo"
           />
         </Link>
         <div className="sP_headerBody">
-          <Search hideButtons={true} inputValue={term["term"]} />
-          <div className="sP_options">
+          <Search hideButtons inputValue={term?.term} />
+
+          <nav className="sP_options">
             <div className="sP_optionsLeft">
-              <div className="sP_option">
-                <Link to="/search" className="active">
-                  All
-                </Link>
-              </div>
-              <div className="sP_option">
-                <Link to="/images">Images</Link>
-              </div>
-              <div className="sP_option">
-                <Link to="/shopping">Shopping</Link>
-              </div>
-              <div className="sP_option">
-                <Link to="/maps">Videos</Link>
-              </div>
-              <div className="sP_option">
-                <Link to="/news">News</Link>
-              </div>
+              {searchPageOptionsLeft.map((label, idx) => (
+                <div className="sP_option" key={idx}>
+                  <Link to={`/${label.toLowerCase()}`}>{label}</Link>
+                </div>
+              ))}
+
               <div className="sP_option_dropdown">
                 <button
                   onClick={() => setShow(!show)}
@@ -51,41 +46,41 @@ function SearchPage() {
                   <MoreVertIcon className="moreVertIcon" />
                   <div>More</div>
                 </button>
-                {show ? <Dropdown show={show} /> : " "}
+                {show ? <Dropdown show={show} items={searchPageMore} /> : " "}
               </div>
             </div>
-            <div className="sP_optionsRight">
-              <div className="sP_option">
-                <Link to="/tools">Tools</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      {term["term"] && (
+            <div className="sP_optionsRight">
+              {searchPageOptionsRight.map((label, idx) => (
+                <div className="sP_option" key={idx}>
+                  <Link to={`/${label.toLowerCase()}`}>{label}</Link>
+                </div>
+              ))}
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      {term?.term && (
         <div className="results">
           <p className="resultsCount">
             About {data?.searchInformation.formattedTotalResults} results (
             {data?.searchInformation.formattedSearchTime} seconds)
           </p>
-          {data?.items.map((item) => (
+          
+          {data?.items?.map((item) => (
             <div className="result" key={item.link}>
               <a href={item.link}>
-                {item.pagemap?.cse_image?.length > 0 &&
-                  item.pagemap?.cse_image[0]?.src && (
-                    <img
-                      className="resultImage"
-                      src={
-                        item.pagemap?.cse_image?.length > 0 &&
-                        item.pagemap?.cse_image[0]?.src
-                      }
-                      alt=""
-                    />
-                  )}
-
+                {item.pagemap?.cse_image?.[0]?.src && (
+                  <img
+                    className="resultImage"
+                    src={item.pagemap.cse_image[0].src}
+                    alt={item.title}
+                  />
+                )}
                 {item.link}
               </a>
+
               <a className="resultTitle" href={item.link}>
                 <h2>{item.title}</h2>
               </a>
